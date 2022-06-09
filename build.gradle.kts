96850273ad13a3bf26c4bc1a9f2e9c04890e3bd4
Roger Viñas <roger.vinas@n26.com>
2022-06-09 17:57:28 +0200

    Add publish task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,21 @@
 plugins {
-    id("org.ajoberstar.grgit") version "4.1.1"
     id("net.researchgate.release") version "2.8.1"
 }
 
-release {
-    pushReleaseVersionBranch = "${grgit.branch.current().getName()}-release"
-    with (propertyMissing("git") as net.researchgate.release.GitAdapter.GitConfig) {
-        requireBranch = ""
+tasks.register("build") {
+    doFirst {
+        println("Building!")
     }
 }
 
-beforeReleaseBuild {
-    doLast {
-        println("Before release!")
+tasks.register("publish") {
+    doFirst {
+        println("Publishing!")
+    }
+}
+
+release {
+    with (propertyMissing("git") as net.researchgate.release.GitAdapter.GitConfig) {
+        requireBranch = ""
     }
 }
