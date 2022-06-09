cab6f7aa020a71fecdb3fd82b22d46b135f83749
Roger Viñas <roger.vinas@n26.com>
2022-06-09 17:20:44 +0200

    Initial import

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("org.ajoberstar.grgit") version "4.1.1"
+    id("net.researchgate.release") version "2.8.1"
+}
+
+
+release {
+    pushReleaseVersionBranch = "${grgit.branch.current().getName()}-release"
+    with (propertyMissing("git") as net.researchgate.release.GitAdapter.GitConfig) {
+        requireBranch = ""
+    }
+}
+
+beforeReleaseBuild {
+    doLast {
+        println("Before release!")
+    }
+}
+
+tasks.register("hello") {
+    doFirst {
+        println("${release.pushReleaseVersionBranch}")
+    }
+}
